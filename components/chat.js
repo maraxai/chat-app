66282f99cfeff292b5d05aa02124440119a07e0c
Mike Dietz <mikedietz@hotmail.com>
2019-08-04 03:16:52 +0200

    layout start screen

--- components/chat.js
+++ components/chat.js
@@ -10,9 +10,18 @@ export default class Chat extends React.Component {
   render() {
     const navigation = this.props.navigation.state.params.name;
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={styles.container}>
     <Text>Hello {this.props.navigation.state.params.name}!!</Text>
     </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue'
+  }
+})
--- components/start.js
+++ components/start.js
@@ -1,46 +1,130 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ImageBackground, TouchableHighlight } from 'react-native';
 
 export default class Start extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: '' }
+    this.state = {
+      name: '',
+      color: ''
+    }
   }
   render() {
     return (
       <View style={styles.container}>
-      <ImageBackground source={'./assets/startscreen_bg.png'} style={{width: 400, height: 200}}>
-      <Text>x</Text>
-      </ImageBackground>
+        <Text style={styles.title}>Chat App</Text>
 
-      <TextInput style={styles.input}/>
-      <Button
-        title="click"
-        onChangeText={(name) => this.setState({name})}
-        value={this.state.name}
-        placeholder='Enter your name...'
-        onPress={() => this.props.navigation.navigate('chat', { name: this.state.name })}
-      />
-    </View>
+        <View style={styles.entries}>
+          <TextInput style={styles.input} placeholder='Your Name'/>
+          <Text style={styles.pickcolor}>Choose Background Color:</Text>
+          <View style={styles.colors}>
+
+            <View style={styles.black}></View>
+            <View style={styles.lila}></View>
+            <View style={styles.blue}></View>
+            <View style={styles.grey}></View>
+          </View>
+          <TouchableHighlight style={styles.button}
+            onChangeText={(name) => this.setState({name})}
+            value={this.state.name}
+            onPress={() => this.props.navigation.navigate('chat', { name: this.state.name })}>
+            <Text style={styles.buttonlabel}>Start Chatting</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
     )
   }
 }
+/*  <ImageBackground source={'./assets/startscreen_bg.png'} style={{width: 400, height: 200}}>
+<Text>x</Text>
+</ImageBackground>
+*/
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#999',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-between',
+    paddingBottom: 30
+  },
+  title: {
+    color: '#fff',
+    fontSize: 45,
+    fontWeight: '600',
+  },
+  pickcolor: {
+    color: '#757083',
+    fontSize: 16,
+    fontWeight: '300',
+    margin: 20
+  },
+  entries: {
+    width: '88%',
+    height: '44%',
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    paddingBottom: 20
   },
   input: {
+    fontSize: 16,
+    fontWeight: '300',
+    color: '#757083',
+    opacity: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#f0f0f0',
+    backgroundColor: '#fff',
     borderWidth: 1,
     borderColor: 'grey',
     margin: 20,
-    width: 200,
+    width: '88%',
+    height: 60,
+    padding: 20
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#757083',
+    width: '88%',
+    height: 60,
+    marginLeft: 18
+  },
+  buttonlabel: {
+    color: '#fff',
+    fontWeight: '600',
+    fontSize: 16
+  },
+  colors: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '88%'
+  },
+  black: {
+    backgroundColor: '#090C08',
+    height: 50,
+    width: 50,
+    borderRadius: 100,
+  },
+  lila: {
+    backgroundColor: '#474056',
+    height: 50,
+    width: 50,
+    borderRadius: 100
+  },
+  blue: {
+    backgroundColor: '#8A95A5',
+    height: 50,
+    width: 50,
+    borderRadius: 100
+  },
+  grey: {
+    backgroundColor: '#B9C6AE',
+    height: 50,
+    width: 50,
+    borderRadius: 100
   }
 });
