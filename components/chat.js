d32c6266f4d320035f4a871b197608c9c5324fa4
Mike Dietz <mikedietz@hotmail.com>
2019-08-12 23:59:02 +0200

    5.4 implement firebase auth and firestore

--- components/chat.js
+++ components/chat.js
@@ -8,33 +8,70 @@ import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 import KeyboardSpacer from 'react-native-keyboard-spacer';
 // gets info from react about the current platform (Android/iOS)
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
 // class component
 export default class Chat extends React.Component {
-  state = {
-    message: []
+  constructor() {
+    super();
+
+    var firebaseConfig = {
+      apiKey: "AIzaSyDAopaFuNd6lMgaAqpuN9YWHD0TSJgZPbA",
+    authDomain: "chat-app-21fd9.firebaseapp.com",
+    databaseURL: "https://chat-app-21fd9.firebaseio.com",
+    projectId: "chat-app-21fd9",
+    storageBucket: "chat-app-21fd9.appspot.com",
+    messagingSenderId: "94521123634",
+    appId: "1:94521123634:web:18053b98f6cf6c2e"
+    };
+
+    if (!firebase.apps.length) { //avoid re-initializing
+      firebase.initializeApp(firebaseConfig)
+    }
+
+    //this.referenceShoppinglistUser = null;
+
+    this.referenceMessages = firebase.firestore().collection('messages');
+
+
+    this.state = {
+      messages: [],
+      uid: 0
+    };
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      var data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt,
+        'user._id': data.user._id,
+        'user.name': data.user.name,
+        'user.avatar': data.user.avatar
+      });
+    });
+    this.setState({
+      messages
+    });
   };
 
-  componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Bonsoir mademe la développeuse!',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 2,
-          text: ' C\'est un message système',
-          createdAt: new Date(),
-          system: true
-        }
-      ],
-    })
+  addMessage() {
+    this.referenceMessages.add({
+      _id: 11,
+      text: 'test',
+      createdAt: '1565646504638',
+      user: {
+        _id: 11,
+        name: 'B.',
+        avatar: 'https://placeimg.com/140/140/any'
+      }
+    });
   }
 
   onSend(messages = []) {
@@ -83,6 +120,26 @@ export default class Chat extends React.Component {
       </View>
     )
   }
+
+    componentDidMount() {
+      this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+      this.setState({
+        uid: user._id,
+      })
+      // create a reference to the active user's documents
+      this.referenceMessage = firebase.firestore().collection('messages').where('uid', '==', this.state._id)
+      // listen for collection changes for current user
+      this.unsubscribeMessageAuthor = this.referenceMessage.onSnapshot(this.onCollectionUpdate);
+    })
+    }
+
+    componentWillUnmount() {
+      this.unsubscribeMessageAuthor();
+      this.authUnsubscribe()
+    }
 }
 
 ////////////
